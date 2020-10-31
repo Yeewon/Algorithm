f76ed0fd5490360f231609425d91ee4422750d6b
Yeewon <39365737+Yeewon@users.noreply.github.com>
2020-10-31 13:25:23 +0900

    Update 영어 끝말잇기.py

--- 영어 끝말잇기.py
+++ 영어 끝말잇기.py
@@ -1,28 +1,24 @@
 def solution(n, words):
-    #번호 : cnt%n; 차례: cnt/n
+    #이전 값을 prev에 담는다.
     prev = words[0]
     for i in range(1, len(words)):
-        word = words[i]
-        if prev[-1] != word[0] or word in words[:i]: #규칙에 어긋나거나 이미 나왔던 단어이면
-            s = '[' + str(i % n + 1) + ',' + str(i // n + 1) + ']'
-            return s
-        else:
-            prev = word
-            continue
-    s = '[0,0]'
-    return s
+        #규칙에 어긋나거나 이전에 나왔던 단어이면
+        if prev[-1] != words[i][0] or words[i] in words[:i]:
+            #몇번째 turn인지 = i//n+1
+            #몇번째 사람인지 = i%n+1
+            return [i%n+1, i//n+1]
+        prev = words[i]
+    return [0,0]
 
 
-'''
 n = 3
 words = ["tank", "kick", "know", "wheel", 'land', 'dream', 'mother', 'robot', 'tank']
-'''
+
 '''
 n = 5
 words = ['hello', 'observe', 'effect', 'take', 'either', 'recognize', 'encourage', 'ensure', 'establish', 'hang', 'gather', 'refer', 'reference', 'estimate', 'executive']
-'''
+
 n = 2
 words = ['hello', 'one','even','never','now','world','draw']
-
-
-print(solution(n,words))
+'''
+print(solution(n, words))
