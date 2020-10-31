4ffa1ad362431bd6591f73282266eaadf73f2d47
Yeewon <39365737+Yeewon@users.noreply.github.com>
2020-10-31 13:26:44 +0900

    Update 배달_우선순위큐 다익스트라.py

--- 배달_우선순위큐 다익스트라.py
+++ 배달_우선순위큐 다익스트라.py
@@ -1,45 +1,44 @@
 import heapq
-INF = int(1e9)
 
-road = [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]]
-n = 5
-k = 3
-
-def dijkstra(graph,start, distance):
+def dijkstra(n, graph, start):
+    INF = int(1e9)
     q = []
+    distance = [INF]*(n+1)
+
     heapq.heappush(q, (0, start))
     distance[start] = 0
 
     while q:
         dist, now = heapq.heappop(q)
+
         if distance[now] < dist:
             continue
-        for i in graph[now]:
+
+        for i in graph[now]: #i = (노드, 거리)
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+                heapq.heappush(q,(cost, i[0]))
+
+    return distance
 
 def solution(n, road, k):
-
-    m = len(road)
-    start = 1
-
-    graph = [[] for i in range(n + 1)]
-    distance = [INF] * (n + 1)
-
+    graph = [[] for i in range(n+1)]
     for data in road:
-        a, b, c = data
-        graph[a].append((b, c))
-        graph[b].append((a, c))
+        a,b,c = data
+        graph[a].append((b,c))
+        graph[b].append((a,c))
 
-    dijkstra(graph, start, distance)
-
-    # 모든 노드로 가기 위한 최단 거리를 출력
+    distance = dijkstra(n, graph, 1)
     cnt = 0
-    for i in range(1, n + 1):
-        if distance[i] <= k:
+    for i in distance:
+        if i <= k:
             cnt += 1
     return cnt
 
+
+
+n = 5
+k = 3
+road = [[1, 2, 1], [2, 3, 3], [5, 2, 2], [1, 4, 2], [5, 3, 1], [5, 4, 2]]
 print(solution(n, road, k))
