ceb307d049b919ade66a495adf313d3aad703973
Yeewon <yes3427@kookmin.ac.kr>
2022-01-09 23:41:51 +0900

    2021.01.09 daily algorithm 구현

---
+++ JavaScript/구현/콜라츠 추측.js
@@ -0,0 +1,11 @@
+function solution(num) {
+  let count = 0;
+  while (count < 500) {
+    if (num === 1) {
+      return count;
+    }
+    num = num % 2 ? num * 3 + 1 : num / 2;
+    count++;
+  }
+  return -1;
+}
