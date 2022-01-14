7ca9b433df570daac09a70a29d522082911d7ed7
Yeewon <yes3427@kookmin.ac.kr>
2022-01-14 23:53:56 +0900

    2021.01.14 daily algorithm 구현

---
+++ JavaScript/구현/문자열 재정렬.js
@@ -0,0 +1,14 @@
+const s = "K1KA5CB7";
+let alpha = "";
+let number = 0;
+
+//  알파벳은 오름차순으로 정렬, 숫자 더한 값은 이어서 출력
+
+for (const char of s) {
+  isNaN(char) ? (alpha += char) : (number += Number(char));
+}
+
+let sortedAlpha = alpha.split("").sort().join("");
+sortedAlpha += String(number);
+
+console.log(sortedAlpha);
