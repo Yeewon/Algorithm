ae68704110b4bb05a3b6dbfbdaa408c533c6e04b
Yeewon <yes3427@kookmin.ac.kr>
2022-02-10 16:24:07 +0900

    음료수 얼려 먹기

--- JavaScript/그래프 탐색/DFS/음료수 얼려 먹기.js
+++ JavaScript/그래프 탐색/DFS/음료수 얼려 먹기.js
@@ -1,11 +1,16 @@
+// 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
 const dfs = (x, y, graph) => {
+  // 주어진 범위를 벗어나는 경우에는 즉시 종료
   if (x <= -1 || x >= n || y <= -1 || y >= m) {
     return false;
   }
 
+  // 현재 노드를 아직 방문하지 않았다면
   if (graph[x][y] === 0) {
+    // 해당 노드를 방문 처리
     graph[x][y] = 1;
 
+    // 상, 하, 좌, 우 의 위치들도 모드 재귀적으로 호출
     dfs(x - 1, y, graph);
     dfs(x + 1, y, graph);
     dfs(x, y - 1, graph);
