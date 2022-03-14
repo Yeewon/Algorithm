7c20d1fccb8b8d28ed99d2e366c726a1fecc746b
Yeewon <yes3427@kookmin.ac.kr>
2022-03-14 23:19:59 +0900

    2022.03.14 daily algorithm 다익스트라

--- JavaScript/최단 경로/간단한 다익스트라.js
+++ JavaScript/최단 경로/간단한 다익스트라.js
@@ -17,19 +17,17 @@ const input = [
 ];
 
 const graph = Array.from({ length: n + 1 }, () => []);
-
-// 방문한 적이 있는지 체크하는 목적의 리스트
-const visited = Array.from({ length: n + 1 }, () => false);
 // 최단 거리 테이블을 모두 무한으로 초기화
 const distance = Array.from({ length: n + 1 }, () => Infinity);
+// 방문한 적이 있는지 체크하는 목적의 리스트
+const visited = Array.from({ length: n + 1 }, () => false);
 
-for (let i = 0; i < m; i++) {
-  const [a, b, c] = input[i];
+input.forEach(([a, b, c]) => {
   graph[a].push([b, c]);
-}
+});
 
 // 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-function get_smallest_node() {
+const get_smallest_node = () => {
   let min_value = Infinity;
   let index = 0;
 
@@ -39,36 +37,35 @@ function get_smallest_node() {
       index = i;
     }
   }
-  return index;
-}
 
-function dijkstra(start) {
+  return index;
+};
+
+const dijkstra = (start) => {
   // 시작 노드에 대해서 초기화, 방문처리
   distance[start] = 0;
   visited[start] = true;
 
-  for (const [dest, dist] of graph[start]) {
+  // 시작노드에 인접한 노드들 거리 입력
+  graph[start].forEach(([dest, dist]) => {
     distance[dest] = dist;
-  }
+  });
 
   // 시작 노드를 제외한 전체 n-1개의 노드에 대해 반복
   for (let i = 0; i < n - 1; i++) {
     // 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
-    now = get_smallest_node();
+    const now = get_smallest_node();
     visited[now] = true;
+
     // 현재 노드와 연결된 다른 노드를 확인
-    for (const [dest, dist] of graph[now]) {
+    graph[now].forEach(([dest, dist]) => {
       const cost = distance[now] + dist;
-      if (cost < distance[dest]) {
-        distance[dest] = cost;
-      }
-    }
+      distance[dest] = Math.min(distance[dest], cost);
+    });
   }
-}
+};
 
 dijkstra(start);
 
 // 모든 노드로 가기 위한 최단 거리를 출력
-for (let i = 1; i < n + 1; i++) {
-  console.log(distance[i] === Infinity ? "INFINITY" : distance[i]);
-}
+distance.forEach((dist) => console.log(dist === Infinity ? "INF" : dist));
