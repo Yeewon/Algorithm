3eccc3a178ec8de2ec26252977d29d896fec4fef
Yeewon <yes3427@kookmin.ac.kr>
2022-03-26 00:42:49 +0900

    2022.03.26 daily algorithm 기타 알고리즘

---
+++ JavaScript/기타 알고리즘/에라토스테네 체.js
@@ -0,0 +1,15 @@
+let sieve = (n) => {
+  let isPrime = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  for (let i = 2; i * i <= n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  return isPrime;
+};
